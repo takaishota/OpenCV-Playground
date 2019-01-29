7ef0f43be323969a77d39a44979a82745ff402bc
高井翔太 <takaishota@gmail.com>
2019-01-29 13:24:33 +0900

    create project

---
+++ OpenCV-Playground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenCV-Playground
+//
+//  Created by 高井　翔太 on 2019/01/29.
+//  Copyright © 2019年 takaishota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
