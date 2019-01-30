ee0c8acec1711a5a81b8316c40543196cf26474c
高井翔太 <takaishota@gmail.com>
2019-01-31 00:40:38 +0900

    add AppRootController

---
+++ OpenCV-Playground/AppRootController.swift
@@ -0,0 +1,43 @@
+//
+//  AppRootController.swift
+//  OpenCV-Playground
+//
+//  Created by 高井　翔太 on 2019/01/31.
+//  Copyright © 2019年 takaishota. All rights reserved.
+//
+
+import UIKit
+
+class AppRootController: UIViewController {
+    var mainNavigationController: UINavigationController?
+
+    var statusBarIsHidden: Bool = true {
+        didSet {
+            setNeedsStatusBarAppearanceUpdate()
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return statusBarIsHidden
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        guard let vc = R.storyboard.main().instantiateInitialViewController() else {
+            return
+        }
+        setInitialViewController(vc)
+    }
+
+    private func setInitialViewController(_ viewController: UIViewController) {
+        let nav = { () -> UINavigationController in
+            let nav = UINavigationController(rootViewController: viewController)
+            nav.setNavigationBarHidden(true, animated: false)
+            return nav
+        }()
+        addChild(nav)
+        view.addSubview(nav.view)
+        mainNavigationController = nav
+        mainNavigationController?.didMove(toParent: self)
+    }
+}
