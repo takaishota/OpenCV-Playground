83c9c49ffd1502cd8faf8bdf4e3c5df83d34d789
ShotaTakai <takaishota@gmail.com>
2019-03-04 23:49:14 +0900

    add Menu Viewcontroller

---
+++ OpenCV-Playground/ViewControllers/MenuViewControllers.swift
@@ -0,0 +1,12 @@
+//
+//  MenuViewControllers.swift
+//  OpenCV-Playground
+//
+//  Created by ShotaTakai on 2019/03/04.
+//  Copyright © 2019 takaishota. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+}
